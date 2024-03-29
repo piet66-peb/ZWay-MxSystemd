35226b7504fc988271c92a04c7b9896ac1664c7c
piet66-peb <dC.16@pm-ebert.de>
2024-03-29 13:26:18 +0100

    1.0.1

--- MxSystemd/sh/exam_coredump.bash
+++ MxSystemd/sh/exam_coredump.bash
@@ -7,14 +7,14 @@
 #h               restarted
 #h Project:      
 #h Installation: edit and install systemd service file z-way-server.service.restart
-#h Usage:        <dir>/exam_coredump.bash [<exam_dir>]
+#h Usage:        <dir>/exam_coredump.bash
 #h Result:       
 #h Examples:     
 #h Outline:      
 #h Resources:    coredumpctl (systemd-coredump), lz4
 #h Platforms:    Linux with systemd/systemctl
 #h Authors:      peb piet66
-#h Version:      V1.0.0 2024-03-28/peb
+#h Version:      V1.0.0 2024-03-29/peb
 #v History:      V1.0.0 2024-03-26/peb first version
 #h Copyright:    (C) piet66 2024
 #h
@@ -24,17 +24,19 @@
 #-----------
 MODULE='exam_coredump.bash'
 VERSION='V1.0.0'
-WRITTEN='2024-03-28/peb'
+WRITTEN='2024-03-29/peb'
 
 #b Variables
 #-----------
 SERVICE=z-way-server
 BASEDIR=`dirname $0`
 PARAMS=${BASEDIR}/params
-EXAM_DIR="$1"
-[ "$EXAM_DIR" == "" ] && EXAM_DIR="$BASEDIR"
+[ -e "$PARAMS" ] && . "$PARAMS"
 
+[ "$EXAM_DIR" == "" ] && EXAM_DIR="$BASEDIR"
+COREDUMP=coredump_decomp
 currtime=$(date +%s)
+COREDUMP_EXAM="${currtime}_coredump_exam"
 
 #b Functions
 #-----------
@@ -67,40 +69,51 @@ function collect_data
 
     echo -e "\n===== gdb:\n"
     #uncompress lz4 file before using with gdb:
-    COREDUMP=coredump_decomp
     [ -e "$COREDUMP" ] && sudo mv -f "$COREDUMP" "$COREDUMP.previous"
     echo sudo lz4 -dfq $STORAGE $COREDUMP
     sudo lz4 -dfq $STORAGE $COREDUMP 
 
     echo sudo gdb $EXECUTABLE $COREDUMP --batch --eval-command="bt full"
     sudo gdb $EXECUTABLE $COREDUMP --batch --eval-command="bt full" 2>/dev/null
-}
+} #collect_data
+
+function send_email
+{
+    logger -is "$1"
+    pushd $BASEDIR >/dev/null 2>&1
+        if [ -e emailAccount.cfg ]
+        then
+            ./sendEmail.bash "$1" "$2" &
+        else
+            echo 'create an emailAccount.cfg file to get a notification'
+        fi
+    popd >/dev/null
+    echo ''
+} #send_email
 
 #b Main
 #------
-sudo systemctl is-failed $SERVICE >/dev/null
+current_state=`sudo systemctl is-failed $SERVICE`
 if [ $? -eq 0 ]
 then
     #b send email
     #------------
-    pushd $BASEDIR >/dev/null 2>&1
-        if [ -e emailAccount.cfg ]
-        then
-            SUBJECT="$SERVICE failed - restarted"
-            CONTENT="examine core dump to \n   ${EXAM_DIR}/${currtime}_coredump_exam"
-            ./sendEmail.bash "$SUBJECT" "$CONTENT" &
-        else
-            echo 'create an emailAccount.cfg file to get a notification' >${currtime}_coredump_exam
-            echo '' >>${currtime}_coredump_exam
-        fi
-    popd >/dev/null
-     
+    SUBJECT="$SERVICE is failed - restarting..."
+    CONTENT="examine core dump to \n   ${EXAM_DIR}/$COREDUMP_EXAM"
+    send_email "$SUBJECT" "$CONTENT" >"$COREDUMP_EXAM"
+
     #b examine core dump
     #-------------------
     [ -d "$EXAM_DIR" ] || mkdir -p "$EXAM_DIR"  
     pushd $EXAM_DIR >/dev/null 2>&1
-        collect_data >>${currtime}_coredump_exam
+        collect_data >>"$COREDUMP_EXAM"
     popd >/dev/null
+else
+    #b send email
+    #------------
+    SUBJECT="$SERVICE($current_state) starting..."
+    CONTENT=""
+    send_email "$SUBJECT" "$CONTENT"
 fi
 exit 0
 
