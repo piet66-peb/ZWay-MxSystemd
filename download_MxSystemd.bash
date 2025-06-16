98fe89e35894930c148cab39f4366f53712d5493
piet66-peb <dC.16@pm-ebert.de>
2025-06-16 11:05:00 +0200

    1.3.3

--- download_MxSystemd.bash
+++ download_MxSystemd.bash
@@ -9,14 +9,14 @@
 #h Usage:        <path>/download_MxSystemd.bash
 #h               or with wget:
 #h               url=https://github.com/piet66-peb/ZWay-MxSystemd/raw/refs/heads/main/download_MxSystemd.bash
-#h               cd /tmp; wget -q -O - $url | sudo bash
+#h               cd /tmp; wget -q -O - $url | bash
 #h Result:       
 #h Examples:     
 #h Outline:      
 #h Resources:    
 #h Platforms:    Linux
 #h Authors:      peb piet66
-#h Version:      V1.1.1 2025-02-20/peb
+#h Version:      V1.2.1 2025-06-16/peb
 #v History:      V1.0.0 2024-10-02/peb first version
 #h Copyright:    (C) piet66 2024
 #h
@@ -25,8 +25,8 @@
 #b Constants
 #-----------
 MODULE='download_MxSystemd.bash'
-VERSION='V1.1.1'
-WRITTEN='2025-02-20/peb'
+VERSION='V1.2.1'
+WRITTEN='2025-06-16/peb'
 
 #b Variables
 #-----------
@@ -47,15 +47,15 @@ echo cd $tmp...
 pushd $tmp >/dev/null
 
 echo downloading $gitzip...
-[ -e "$gitzip" ] && sudo rm $gitzip
+[ -e "$gitzip" ] && rm $gitzip
 wget -nv -O $gitzip $url
 
 echo extracting $gitzip...
-[ -e "$module" ] && sudo rm -R $module
-sudo unzip -q -o $gitzip
+[ -e "$module" ] && rm -R $module
+unzip -q -o $gitzip
 
 echo copying $pack to $tardir...
-sudo cp -dpR $module $tardir
+cp -dpR $module $tardir
 
 echo done.
 popd >/dev/null
